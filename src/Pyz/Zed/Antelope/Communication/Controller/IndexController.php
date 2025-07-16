0e6d90a3d8344fb8f1f3604947041e37e5b86019
Ionut Dobrotescu <ionut.dobrotescu@nagarro.com>
2025-07-16 13:39:39 +0300

    explorer - task 2 - Antelope Location reader and writers.

--- src/Pyz/Zed/Antelope/Communication/Controller/IndexController.php
+++ src/Pyz/Zed/Antelope/Communication/Controller/IndexController.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\Antelope\Communication\Controller;
 
 use Faker\Factory as FakerFactory;
+use Generated\Shared\Transfer\AntelopeLocationTransfer;
 use Generated\Shared\Transfer\AntelopeTransfer;
 use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,6 +19,9 @@ class IndexController extends AbstractController
      */
     public function addAction(Request $request): array
     {
+        $antelopeLocationTransfer = new AntelopeLocationTransfer();
+        $antelopeLocationTransfer->setLocationName(FakerFactory::create()->name());
+        $antelopeLocationTransfer = $this->getFacade()->createAntelopeLocation($antelopeLocationTransfer);
 
         $antelopeTransfer = new AntelopeTransfer();
         $name = $request->get('name');
@@ -25,7 +29,14 @@ class IndexController extends AbstractController
             $name = FakerFactory::create()->name() ;
         }
         $antelopeTransfer->setName($name);
+        $antelopeTransfer->setFkAntelopeLocation($antelopeLocationTransfer->getIdLocation());
         $this->getFacade()->createAntelope($antelopeTransfer);
-        return $this->viewResponse(['antelope' => $antelopeTransfer]);
+
+        $locationTransfer = $this->getFacade()->getAntelopeLocationById($antelopeLocationTransfer->getIdLocation());
+
+        return $this->viewResponse([
+            'antelope' => $antelopeTransfer,
+            'antelopeLocation' => $locationTransfer,
+        ]);
     }
 }
